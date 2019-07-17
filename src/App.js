0100b802a83083ee676b82e20a6639170e718dea
Alex Poulos <mapoulos@umd.edu>
2019-07-17 11:04:59 -0400

    started on timer class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class Timer {
+	constructor() {
+		this.elapsedTime = 0
+		this.timerLength = 15*60*1000
+		this.isRunning = false
+		this.timeStarted = 0
+		this.onFinish = () => {}
+		this.intervalId = 0
+	}
+
+	startTimer() {
+		this.isRunning = true
+		var elapsedTime = this.elapsedTime
+		this.timeStarted = Date.now()
+		this.intervalId = window.setInterval((timer) => {
+				timer.elapsedTime = Date.now() - timer.elapsedTime
+				if(elapsedTime >= timer.timerLength()) {
+					timer.stopTimer()
+					timer.onFinish()
+				}
+			}, 1000, this)
+	}
+
+
+	pauseTimer() {
+		this.isRunning = false
+		window.clearInterval(this.intervalId)
+	}
+
+	stopTimer() {
+		this.elapsedTime = 0
+		window.clearInterval(this.intervalId)
+	}
+
+
+
+}
+
+class TimerUI extends React.Component {
+	constructor(props) {
+
+		//state:
+		//-timer length
+		//-elapsed time
+		//-number of breaks
+		//-timeToNextBreak
+		//
+	}
+	
+}
+
+
+class App extends React.Component {
+	
+
+	render() {
+		return (
+			<Container>
+			</Container>
+		);
+	}
 }
 
 export default App;
