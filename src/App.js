c141d2cafdd6b66a7ff1ba56a112c1ceeb60b9b5
Alex Poulos <mapoulos@umd.edu>
2019-07-17 14:25:04 -0400

    started adding timer ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 
 
@@ -45,13 +50,53 @@ class Timer {
 
 class TimerUI extends React.Component {
 	constructor(props) {
+		super(props)
 
-		//state:
-		//-timer length
-		//-elapsed time
-		//-number of breaks
-		//-timeToNextBreak
-		//
+		this.timer = new Timer()
+		this.state = {
+			elapsedTime: 0,
+			timerLength: 15*60*1000, //15 minutes in ms
+			paused: false,
+			started: false
+		}
+
+		this.convertMillisecondsToString = this.convertMillisecondsToString.bind(this)
+	}
+
+	convertMillisecondsToString(ms) {
+		//get minutes
+		let mins = Math.floor(ms / 60 / 1000);
+		let secs = Math.floor(ms / 1000);
+		var minStr = "" + mins;
+		var secStr = "" + secs;
+		if(mins < 10) {
+			minStr = "0" + minStr
+		}
+		if(secs < 10) {
+			secStr = "0" + secs;
+		}
+		return minStr + ":" + secStr;
+	}
+
+	render() {
+		return(
+			<Container lg={12}>
+				<Row style={{marginTop: "10%"}}>
+				<Col>
+					
+				<Card className="text-center">
+				<Card.Title>{this.convertMillisecondsToString(this.state.elapsedTime)}</Card.Title>
+				</Card>
+				</Col>
+				</Row>
+
+				
+				<Row>
+				<Col><Button /></Col><Col><Button /> </Col>
+				</Row>
+			</Container>
+
+			);
 	}
 	
 }
@@ -62,8 +107,7 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<Container>
-			</Container>
+			<TimerUI />
 		);
 	}
 }
