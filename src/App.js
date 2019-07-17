95be315b6a496c3af9e28f076a3757891cf2e4e0
Alex Poulos <mapoulos@umd.edu>
2019-07-17 16:39:11 -0400

    the basic mechanics of the timer seem to be working, just need to handle the pausing better. getting close to having something usable, even if it isnt pretty yet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
+import playImage from './images/icons8-play-50.png'
+import pauseImage from './images/icons8-pause-50.png'
+import stopImage from './images/icons8-stop-50.png'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
 
 
 
@@ -17,30 +21,40 @@ class Timer {
 		this.isRunning = false
 		this.timeStarted = 0
 		this.onFinish = () => {}
+		this.onUpdate = () => {}
 		this.intervalId = 0
 	}
 
-	startTimer() {
+	start() {
+		if(this.isRunning === false) {
 		this.isRunning = true
 		var elapsedTime = this.elapsedTime
 		this.timeStarted = Date.now()
 		this.intervalId = window.setInterval((timer) => {
-				timer.elapsedTime = Date.now() - timer.elapsedTime
-				if(elapsedTime >= timer.timerLength()) {
-					timer.stopTimer()
+				timer.elapsedTime = Date.now() - timer.timeStarted
+				if(elapsedTime >= timer.timerLength) {
+					timer.stop()
+
 					timer.onFinish()
+					
+				} else {
+					timer.onUpdate()
 				}
+
 			}, 1000, this)
+		}
 	}
 
 
-	pauseTimer() {
+	pause() {
 		this.isRunning = false
 		window.clearInterval(this.intervalId)
 	}
 
-	stopTimer() {
+	stop() {
+		this.isRunning = false
 		this.elapsedTime = 0
+		this.onUpdate()
 		window.clearInterval(this.intervalId)
 	}
 
@@ -53,11 +67,18 @@ class TimerUI extends React.Component {
 		super(props)
 
 		this.timer = new Timer()
+		this.timer.onFinish = () => {
+			this.setState({elapsedTime: 0})
+		}
+		this.timer.onUpdate = () => {
+			this.setState({elapsedTime: this.timer.elapsedTime})
+		}
 		this.state = {
 			elapsedTime: 0,
 			timerLength: 15*60*1000, //15 minutes in ms
 			paused: false,
-			started: false
+			started: false,
+			playImage: playImage
 		}
 
 		this.convertMillisecondsToString = this.convertMillisecondsToString.bind(this)
@@ -66,7 +87,7 @@ class TimerUI extends React.Component {
 	convertMillisecondsToString(ms) {
 		//get minutes
 		let mins = Math.floor(ms / 60 / 1000);
-		let secs = Math.floor(ms / 1000);
+		let secs = Math.floor(ms/1000) % 60;
 		var minStr = "" + mins;
 		var secStr = "" + secs;
 		if(mins < 10) {
@@ -81,18 +102,24 @@ class TimerUI extends React.Component {
 	render() {
 		return(
 			<Container lg={12}>
-				<Row style={{marginTop: "10%"}}>
-				<Col>
-					
-				<Card className="text-center">
-				<Card.Title>{this.convertMillisecondsToString(this.state.elapsedTime)}</Card.Title>
-				</Card>
+				<Row style={{marginTop: "10%", textAlign: "center"}}>
+
+				<Col lg={{span:1, offset:5, marginLeft: "10%"}}>
+					<h1>{this.convertMillisecondsToString(this.state.timerLength-this.state.elapsedTime)}</h1>
+			
 				</Col>
 				</Row>
 
 				
 				<Row>
-				<Col><Button /></Col><Col><Button /> </Col>
+				<Col lg={{span:1, offset: 5}}><Button variant="secondary"><Image onClick={() => {
+						//handle this logic better for pausing, etc.
+						this.timer.start();
+						this.setState((prevState) => (
+							{playImage: pauseImage}))
+					}
+				} src={this.state.playImage} /></Button></Col>
+				<Col lg={1} ><Button variant="secondary"><Image onClick={() => {this.timer.stop()}} src={stopImage}/></Button></Col>
 				</Row>
 			</Container>
 
